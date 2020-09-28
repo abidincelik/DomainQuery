ea0124a3e2c3ae402a97a7a9ab0f788139813e1a
Abidin Çelik <abidincelik@msn.com>
2020-09-28 16:05:18 +0300

    sorgu servisi eklendi

--- src/App.js
+++ src/App.js
@@ -3,29 +3,51 @@ import './App.css'
 
 export default class App extends Component {
 
-  componentDidMount(){
-    var str= 'aaa';
-
-    while(str!=='zzz') {
-    str= ((parseInt(str, 36)+1).toString(36)).replace(/0/g,'0'); // 0 yerine a yazılırsa alfabetik oluşur 
-
-    var node = document.createElement("p");                
-    var textnode = document.createTextNode("Bulunan Domain : " + str);        
-    node.appendChild(textnode);                             
-    document.getElementById("domainlist").appendChild(node); 
-
+  constructor() {
+    super();
+    this.state = {
+      domain: []
     }
   }
+
+  componentDidMount() {
+    var str = 'a';
+
+    while (str !== 'zzz') {
+      str = ((parseInt(str, 36) + 1).toString(36)).replace(/0/g, '0'); // 0 yerine a yazılırsa alfabetik oluşur 
+      this.state.domain.push(str);
+    };
+
+    this.state.domain.forEach((element, index) => { 
+          setTimeout(() => {
+           this.domainsorgu(element);
+          }, 300 *(index+1)) // or just index, depends on your needs
+           
+  });
+  }
+
+  domainsorgu(str) {
+   fetch('https://rdap.verisign.com/com/v1/domain/' + str + '.com')
+     .then(response => {
+        if (response.ok === false) {
+         console.log(str + '.com');
+         var node = document.createElement("p");
+         var textnode = document.createTextNode("Bulunan Domain : " + str + ".com");
+         node.appendChild(textnode);
+         document.getElementById("domainlist").appendChild(node);
+       }
+     });
+  };
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <p id="domainlist">
-          Domain Query
+        <header className="App-header">
+          <p id="domainlist">
+            Domain Query
         </p>
-      </header>
-    </div>
+        </header>
+      </div>
     )
   }
 }
