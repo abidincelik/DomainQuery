c2106d0371733fdded893ead578f619e8d877901
Abidin Çelik <abidincelik@msn.com>
2020-09-28 16:18:15 +0300

    son hali

--- src/App.js
+++ src/App.js
@@ -11,41 +11,43 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    var str = 'a';
+    var str = 'aaaa';
 
-    while (str !== 'zzz') {
+    while (str !== 'zzzz') {
       str = ((parseInt(str, 36) + 1).toString(36)).replace(/0/g, '0'); // 0 yerine a yazılırsa alfabetik oluşur 
       this.state.domain.push(str);
     };
 
-    this.state.domain.forEach((element, index) => { 
-          setTimeout(() => {
-           this.domainsorgu(element);
-          }, 300 *(index+1)) // or just index, depends on your needs
-           
-  });
+    this.state.domain.forEach((element, index) => {
+      setTimeout(() => {
+        document.getElementById("denenen").innerText = element + '.com';
+        this.domainsorgu(element);
+      }, 300 * (index + 1))
+    });
   }
 
   domainsorgu(str) {
-   fetch('https://rdap.verisign.com/com/v1/domain/' + str + '.com')
-     .then(response => {
+    fetch('https://rdap.verisign.com/com/v1/domain/' + str + '.com')
+      .then(response => {
         if (response.ok === false) {
-         console.log(str + '.com');
-         var node = document.createElement("p");
-         var textnode = document.createTextNode("Bulunan Domain : " + str + ".com");
-         node.appendChild(textnode);
-         document.getElementById("domainlist").appendChild(node);
-       }
-     });
+          //console.log(str + '.com');
+          var node = document.createElement("p");
+          var textnode = document.createTextNode("Bulunan Domain : " + str + ".com");
+          node.appendChild(textnode);
+          document.getElementById("domainlist").appendChild(node);
+        }
+      });
   };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <p id="denenen">
+          </p>
           <p id="domainlist">
             Domain Query
-        </p>
+          </p>
         </header>
       </div>
     )
